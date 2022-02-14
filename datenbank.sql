3ce46bb471d32d9a41d179ed57efa1f8b028ad09
Balázs Megyeri <megyerib@users.noreply.github.com>
2022-02-14 20:25:18 +0100

    DB CREATE hozzáadva az sql-hez

--- datenbank.sql
+++ datenbank.sql
@@ -19,6 +19,9 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `info2`
 --
+CREATE DATABASE IF NOT EXISTS `info2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `info2`;
+--
 
 -- --------------------------------------------------------
 
